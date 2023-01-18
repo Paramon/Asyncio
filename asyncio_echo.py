51cb1d623876d650868a339c1145dca7e9afcfac
Andrii Paramonov <andrii.paramonov@gmail.com>
2023-01-18 22:02:16 +0200

    comments

--- asyncio_echo.py
+++ asyncio_echo.py
@@ -15,10 +15,15 @@ async def echo(connection: socket,loop: AbstractEventLoop) -> None:
     :type loop: AbstractEventLoop
     """
     try:
+        # Receiving data from the client.
         while data := await loop.sock_recv(connection, 1024):
             print("got data!")
+            # Checking if the data that was received from the client is equal to the string
+            # 'boom\r\n'.
             if data == b'boom\r\n':
+                # It raises an exception.
                 raise Exception("Неожиданная ошибка сети")
+            # It sends data to the client.
             await loop.sock_sendall(connection, data)
     except Exception as ex:
         logging.exception(ex)
@@ -29,44 +34,45 @@ async def echo(connection: socket,loop: AbstractEventLoop) -> None:
 echo_tasks = []
 
 
-async def connection_listener(server_socket: socket,
-                                loop: AbstractEventLoop):
+async def connection_listener(server_socket: socket,loop: AbstractEventLoop):
     """
-    It listens for new connections on the server socket, and when it gets one, it creates a new socket
-    for that connection, and then passes that socket to the `connection_handler` function
+    It accepts a socket and an event loop, and then it waits for a connection to be made to the socket.
+    When a connection is made, it creates a new task that will handle the connection
     
-    :param server_socket: The socket that the server is listening on
+    :param server_socket: the socket object that will be used to accept incoming connections
     :type server_socket: socket
-    :param loop: The event loop that the server is running on
+    :param loop: the event loop that will be used to run the server
     :type loop: AbstractEventLoop
     """
+   
     while True:
+        # Waiting for a connection to be made to the socket.
         connection, address = await loop.sock_accept(server_socket)
+        # It sets the socket to non-blocking mode.
         connection.setblocking(False)
         print(f"Получен запрос на подключение {address}")
+        # It creates a new task that will handle the connection.
         asyncio.create_task(echo(connection, loop))
 
 
-# It's a subclass of SystemExit that doesn't print a stack trace
+
+# Это подкласс SystemExit, который не печатает трассировку стека.
 class GracefulExit(SystemExit):
     pass
 
 def shutdown():
-    """
-    It shuts down the script.
-    """
     raise GracefulExit()
 
 
 async def close_echo_task(echo_tasks: List[asyncio.Task]):
     """
-    "Wait for all echo tasks to complete, but if they don't complete in 2 seconds, cancel them."
+    «Дождитесь завершения всех эхо-задач, но если они не завершатся в течение 2 секунд, отмените их».
     
-    The first line of the function creates a list of tasks that we want to wait for. The second line
-    creates a list of waiters. A waiter is a task that waits for another task to complete. In this case,
-    we're waiting for each echo task to complete
+    Первая строка функции создает список задач, которые мы хотим дождаться. Вторая строка создает список
+    официантов. Официант — это задача, которая ожидает завершения другой задачи. В этом случае мы ждем
+    завершения каждой эхо-задачи.
     
-    :param echo_tasks: List[asyncio.Task]
+    :param echo_tasks: Список[asyncio.Task]
     :type echo_tasks: List[asyncio.Task]
     """
     waiters = [asyncio.wait_for(task, 2) for task in echo_tasks]
@@ -82,12 +88,19 @@ async def main():
     """
     A function that is asynchronous.
     """
+    
+    # It creates a socket object that will be used to accept incoming connections.
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # It sets the socket to reuse the address.
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+    # Creating a tuple that contains the IP address and port number that the server will listen on.
     server_address = ('127.0.0.1', 8000)
+    # It sets the socket to non-blocking mode.
     server_socket.setblocking(False)
+    # It binds the socket to the IP address and port number that the server will listen on.
     server_socket.bind(server_address)
+    # It tells the socket to start listening for incoming connections.
     server_socket.listen()
 
     for signame in {'SIGINT', 'SIGTERM'}:
@@ -95,13 +108,16 @@ async def main():
     await connection_listener(server_socket, loop)
 
 
-# It's a common idiom in Python to use this statement to check if the code is being run as a script,
-# or if it's being imported as a module.
+
+
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     try:
+        # It creates an event loop.
         loop.run_until_complete(main())
     except GracefulExit:
+        # Waiting for all the echo tasks to complete, but if they don't complete within 2 seconds, it
+        # cancels them.
         loop.run_until_complete(close_echo_task(echo_tasks))
     finally:
         loop.close()
