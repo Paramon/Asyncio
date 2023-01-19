d752e6c9956d8ff2ea02dde70e42079dfe650e5f
Andrii Paramonov <andrii.paramonov@gmail.com>
2023-01-19 22:19:52 +0200

    timeout

--- aiohttp_web_request.py
+++ aiohttp_web_request.py
@@ -15,14 +15,18 @@ async def fetch_status(session: ClientSession, url: str) -> int:
     :type url: str
     :return: The status code of the url
     """
-    async with session.get(url) as result:
+    # Creating a timeout object that will be used to set the timeout for the request.
+    ten_milliseconds = aiohttp.ClientTimeout(total=.1)
+    async with session.get(url, timeout=ten_milliseconds) as result:
         return result.status
 
 
 @async_timed()
 async def main():
     # Creating a session object that we can use to make requests.
-    async with aiohttp.ClientSession() as session:
+    # Setting the timeout for the session to 1 second.
+    session_timeout = aiohttp.ClientTimeout(total=1, connect=.1)
+    async with aiohttp.ClientSession(timeout=session_timeout) as session:
         url = 'http://www.example.com'
         # Calling the `fetch_status` function and waiting for it to finish.
         status = await fetch_status(session, url)
